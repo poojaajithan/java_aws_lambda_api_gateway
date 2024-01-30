5bbbba015fad9d89f7d5e672450ad4a11b16739d
Pooja Ajithan <poojaajithan91@gmail.com>
2024-01-30 14:19:44 +0530

    Update Employee.java

--- src/main/java/com/awsprojects/poc/java_aws_lambda_api_gateway/Employee.java
+++ src/main/java/com/awsprojects/poc/java_aws_lambda_api_gateway/Employee.java
@@ -1,1 +1,13 @@
+package com.awsprojects.poc.java_aws_lambda_api_gateway;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Employee {
+	private Integer empId;
+	private String empName;
+	private String email;
+	
+}
